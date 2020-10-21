3b1e33b4c4d6437a61314d09d588ad40b206c6d3
FrancoQuezada1 <francojere14@gmail.com>
2020-10-21 20:28:38 -0300

    Agregando comentario

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { StatusBar } from "expo-status-bar";
+/* Autor Franco 21/10/20 Instituto Superior Patagonico */
+import { StatusBar } from "expo-status-bar"; 
 import React, { useState } from "react";
 import {
   ActivityIndicator,
