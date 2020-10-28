dce46a93334826bca4863239fb2629733fa8d356
Facu Crespo <facu@mail.com>
2020-10-27 22:56:22 -0300

    prueba1

--- App.js
+++ App.js
@@ -4,90 +4,34 @@
   21/10/20
    */
 
-
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import {
-  ActivityIndicator,
-  Button,
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import Boton from './componentes/Boton';
-
-export default function App() {
-  const [countClick, setCount] = useState(0);
-  const [loading, setLoader] = useState(false);
-  const [clicks,setClicks] = useState([])
-
-
-  const pressFunction = () => {
-    setLoader(true);
-    const count = countClick + 1;
-    const t = new Date();
-    const time = t.getTime();
-    const Newclick = [...clicks,{number:count,time:time}] 
-
-    setCount(count);
-    setClicks(Newclick);
-    setTimeout(() => {
-      setLoader(false);
-    }, 1000);
-  };
-
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text>You press {countClick} times the button! </Text>
-        <Boton titulo="cambia de color"/>
-        
-        <Button
-          title={
-            !loading ? (
-              "Click Me"
-            ) : (
-              <ActivityIndicator
-                size="small"
-                color="white"
-                animating={loading}
-              />
-            )
-          }
-          onPress={pressFunction}
-          disabled={loading}
+ 
+  
+ import React from "react";
+import { StyleSheet, View } from "react-native";
+  import { Avatar, Accesory } from "react-native-elements";
+  
+  export default function App() {
+    return (
+      <View style={styles.container}>
+      <Avatar
+          rounding
+          size="xlarge"
+          title="ID"
+          activeOpacity={0.7}
+          containerStyle={styles.avatar}
+          titleStyle={styles.icon}
         />
       </View>
-      <ListExample clicks = {clicks}/>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const Click = ({click}) => (
-  <View>
-  <Text>{`Click number ${click.number} at the ${click.time}`}</Text>
-  </View>
-  );
-
-const ListExample = ({clicks}) => {
+    );
+  }
   
-  const renderItems = ({item}) => (
-    
-     <Click click={item} />
-  )
-
-  return <View>
-  <FlatList data={clicks} renderItem = {renderItems} keyExtractor={click => click.number}  />
-  </View>;
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "black",
+      color:"black"
+    });
+    avatar:{
+        backgroundColor:"white"
+        color:"white"
+  });
